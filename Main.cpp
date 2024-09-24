a97400feaa7ef65b0884a6189c8718d0080a0100
jruiz002 <josegrg04@gmail.com>
2024-09-24 07:30:07 -0600

    Se creó la estructura del programa del ejercicio 1.

---
+++ Main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// Estructura para almacenar la información de cada producto
+struct Producto {
+    string nombre;
+    double precioUnitario;
+    double costoUnitario;
+    int cantidadVendida;
+};
+
+// Función para calcular el monto de venta por producto
+double calcularMontoVentas(const Producto& producto) {
+    return 0;
+}
+
+// Función para calcular la utilidad por producto
+double calcularUtilidadPorProducto(const Producto& producto) {
+    return 0;
+}
+
+// Función para calcular el monto total de ventas del mes
+double calcularMontoTotalVentas(const vector<Producto>& productos) {
+    return 0;
+}
+
+// Función para calcular la utilidad total del mes
+double calcularUtilidadTotalMes(const vector<Producto>& productos) {
+    return 0;
+}
+
+int main() {
+
+    return 0;
+}
