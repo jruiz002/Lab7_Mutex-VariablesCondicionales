72e8fe14c24e023701af59f5ace0d660b9809ac2
jruiz002 <josegrg04@gmail.com>
2024-09-24 18:53:10 -0600

    Se modificó la estructura de trabajo del ejercicio 1

--- Main.cpp
+++ Main.cpp
@@ -14,36 +14,23 @@ struct Producto {
     double utilidadProducto;
 };
 
-// Función para calcular el monto de venta por producto
-double calcularMontoVentas(int unidadesVendidas, double precioUnitario) {
-    return unidadesVendidas * precioUnitario;
-}
-
-// Función para calcular la utilidad por producto
-double calcularUtilidadPorProducto(double ventasProducto, int unidadesVendidas, double costoUnitario) {
-    return ventasProducto - (unidadesVendidas * costoUnitario);
-}
-
 // Variables globales
 Producto* productosMes;
 int numProductos;
-pthread_mutex_t candado = PTHREAD_MUTEX_INITIALIZER;   // Mutex para proteger acceso a los datos
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;      // Variable condicional
-int hilosTerminados = 0;                             // Contador de hilos terminados
+pthread_mutex_t candado = PTHREAD_MUTEX_INITIALIZER;  
+int hilosTerminados = 0;                           
 
 // Función que será ejecutada por cada hilo
 void* calcularVentasYUtilidades(void* arg) {
     int idx = *(int*)arg;
 
-    // Imprimir cuando el hilo inicia
     cout << "Cálculo hilo " << idx << " iniciado" << endl;
 
     // Simular el tiempo de cálculo
     sleep(1);
 
-    // Calcular ventas y utilidades del producto
-    double ventas = calcularMontoVentas(productosMes[idx].cantidadVendida, productosMes[idx].precioUnitario);
-    double utilidad = calcularUtilidadPorProducto(ventas, productosMes[idx].cantidadVendida, productosMes[idx].costoUnitario);
+    double ventas = productosMes[idx].cantidadVendida * productosMes[idx].precioUnitario;
+    double utilidad = ventas - (productosMes[idx].cantidadVendida * productosMes[idx].costoUnitario);
 
     // Bloquear el mutex antes de modificar los datos compartidos
     pthread_mutex_lock(&candado);
@@ -51,16 +38,9 @@ void* calcularVentasYUtilidades(void* arg) {
     productosMes[idx].ventasProducto = ventas;
     productosMes[idx].utilidadProducto = utilidad;
     
-    // Incrementar el número de hilos que han terminado
     hilosTerminados++;
 
-    // Imprimir cuando el hilo termina
     cout << "Cálculo hilo " << idx << " terminado" << endl;
-
-    // Si todos los hilos han terminado, señalizar la condición
-    if (hilosTerminados == numProductos) {
-        pthread_cond_signal(&cond);
-    }
 
     // Desbloquear el mutex después de modificar los datos
     pthread_mutex_unlock(&candado);
@@ -87,30 +67,11 @@ double calcularUtilidadTotalMes() {
 }
 
 // Función para procesar los productos de un mes
-void procesarMes(string nombreMes, Producto productos[], int numProd) {
-    productosMes = productos;
-    numProductos = numProd;
-    hilosTerminados = 0;
-
-    // Crear un hilo por cada producto
-    pthread_t hilos[numProductos];
-    int indices[numProductos];
-
-    for (int i = 0; i < numProductos; ++i) {
-        indices[i] = i;
-        pthread_create(&hilos[i], NULL, calcularVentasYUtilidades, &indices[i]);
-    }
-
-    // Bloquear el mutex para esperar la señal de la variable condicional
-    pthread_mutex_lock(&candado);
-
-    // Esperar a que todos los hilos terminen
+void procesarMes(string nombreMes) {
+    // Esperar a que todos los hilos terminen antes de imprimir
     while (hilosTerminados < numProductos) {
-        pthread_cond_wait(&cond, &candado);
+        sleep(1);
     }
-
-    // Desbloquear el mutex una vez que todos los hilos hayan terminado
-    pthread_mutex_unlock(&candado);
 
     // Imprimir el reporte del mes
     cout << "------------------------------------------" << endl;
@@ -133,8 +94,31 @@ void procesarMes(string nombreMes, Producto productos[], int numProd) {
     cout << "------------------------------------------" << endl;
 }
 
+void procesarProductos(Producto productos[], int numProd, string nombreMes) {
+    productosMes = productos;
+    numProductos = numProd;
+    hilosTerminados = 0;  // Reiniciar contador de hilos terminados
+
+    // Crear un hilo por cada producto
+    pthread_t hilos[numProductos];
+    int indices[numProductos];
+
+    for (int i = 0; i < numProductos; ++i) {
+        indices[i] = i;
+        pthread_create(&hilos[i], NULL, calcularVentasYUtilidades, &indices[i]);
+    }
+
+    // Procesar el mes (esperar a que los hilos terminen e imprimir)
+    procesarMes(nombreMes);
+
+    // Unir todos los hilos (esperar a que todos los hilos finalicen)
+    for (int i = 0; i < numProductos; ++i) {
+        pthread_join(hilos[i], NULL);
+    }
+}
+
 int main() {
-    // Datos de los productos para julio
+    // Datos de los productos para los meses
     Producto productosJulio[] = {
         {"Porción pastel de chocolate", 60, 20, 300},
         {"White mocha", 32, 19.20, 400},
@@ -146,7 +130,6 @@ int main() {
         {"Café tostado molido",60, 20, 60}
     };
 
-    // Datos de los productos para agosto
     Producto productosAgosto[] = {
         {"Porción pastel de chocolate", 60, 20, 250},
         {"White mocha", 32, 19.20, 380},
@@ -158,15 +141,14 @@ int main() {
         {"Café tostado molido",60, 20, 15}
     };
 
-    // Procesar julio
-    procesarMes("JULIO", productosJulio, 8);
+    // Procesar los productos de Julio
+    procesarProductos(productosJulio, 8, "JULIO");
 
-    // Procesar agosto
-    procesarMes("AGOSTO", productosAgosto, 8);
+    // Procesar los productos de Agosto
+    procesarProductos(productosAgosto, 8, "AGOSTO");
 
-    // Destruir los mutex y variables condicionales
+    // Destruir el mutex
     pthread_mutex_destroy(&candado);
-    pthread_cond_destroy(&cond);
 
     return 0;
 }
